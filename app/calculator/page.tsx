8ead269d62f254cad2b56d873d70bb6dfd6554b2
jjliu168 <jjliu3@asu.edu>
2024-09-27 07:42:31 -0700

    Added: some extra pages clearing out some of the initial Next.js and  Green Expectations Logo

---
+++ app/calculator/page.tsx
@@ -0,0 +1,16 @@
+// app/calculator/page.tsx
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+const CalculatorPage = () => {
+  return (
+    <div>
+      <h1>Calculator</h1>
+      <p>This is a basic calculator placeholder page.</p>
+      {/* You can extend this with actual calculator functionality later */}
+    </div>
+  );
+};
+
+export default CalculatorPage;
---
+++ app/chatbot/page.tsx
@@ -0,0 +1,17 @@
+// app/chatbot/page.tsx
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+const ChatbotPage = () => {
+    return(
+        <div>
+            <h1>Chatbot</h1>
+            <p>This is the placeholder page for the chatbot</p>
+            {/* add additional chatbot functionality here */}
+
+        </div>
+    )
+}
+// Exports the chatbot for availability in other parts of the app
+export default ChatbotPage;
