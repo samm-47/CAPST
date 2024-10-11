59d14be6b996a12c21b316c89ef5c3c411d034f3
Jared Liu <jettliu1688@gmail.com>
2024-10-11 11:38:27 -0700

    Updated Basic layout of Nav Bar - Moved Icon to leftside - Added the text and oriented to match Greenify AI

--- app/faq/faq_layout.tsx
+++ app/faq/faq_layout.tsx
@@ -27,23 +27,27 @@ export default function ClientLayout({ children }: { children: React.ReactNode }
   return (
     <div className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
       {/* Top navigation bar with logo */}
-      <div className="bg-gray-100 h-16 flex items-center justify-center px-8 sticky top-0 z-50"> {/* z-index deals with what elements eppear on top of others */}
+      <div className="bg-white h-14 flex items-center justify-start px-8 sticky top-0 z-50 shadow-md"> {/* z-index deals with what elements eppear on top of others */}
         <Link href="/" passHref>
           <Image
             src="/green_expectations_logo.png"
             alt="Green Expectations Logo"
-            width={50}
-            height={50}
+            width={35}
+            height={35}
             className="transition hover:grayscale hover:brightness-90"
             priority
           />
         </Link>
+        {/* Create a multi-line entry for Green Expectations nav like they have on  their site */}
+        <div className="leading-tight">{/* this wraps the text  */}
+          <span className="ml-2 block text-med font-semibold">Green</span> {/* span is an inline element  */}
+          <span className="ml-2 text-med">Expectations</span>{/* span is an inline element  */}
+        </div>
+
       </div>
 
       {/* Main content area */}
-      <main>
-        {children}
-      </main>
+      <main>{children}</main>
     </div>
   );
 }
