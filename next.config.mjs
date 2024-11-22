803d2504fa0edc5c3bb78ab0ff76d2783d711ab1
samm-47 <96743711+samm-47@users.noreply.github.com>
2024-11-21 20:55:36 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: process.env.NODE_ENV === 'production' ? 'export' : undefined, // allows for working with npm run dev and npx next build
-    images: {
-      unoptimized: process.env.NODE_ENV === 'production', // Only apply in production
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    unoptimized: process.env.NODE_ENV === 'production', // Only apply in production
+  },
+};
+
+export default nextConfig;
