ae5b7d721d3cfe137a00f544d2a54c8c20c018c6
samm-47 <96743711+samm-47@users.noreply.github.com>
2024-11-22 00:18:15 -0700

    Update next.config.mjs try1

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,8 @@ export default {
   async rewrites() {
     return [
       {
-        source: '/api/:path*',
-        destination: 'https://capst.onrender.com/api/:path*', // Proxy to the Flask backend
+        source: '/api/chat',
+        destination: 'https://capst.onrender.com/api/chat', // Proxy to the Flask backend
       },
     ];
   },
